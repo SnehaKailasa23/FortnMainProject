5a30c988efad0d18522cc30e9d4857b3e2c87161
GGK - Yerriswamy Konanki <GGK-ykonanki@users.noreply.padlgithubggk1.sw.fortna.net>
2017-12-19 16:53:26 +0530

    updated with version number

--- CD/deployment.sh
+++ CD/deployment.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 ############replacing docker-stack.yml varibles
+
+##$1 is environment varible
+##$2 is version number
+##$3 is app name
+
 for i in `grep -e $1 -e global environment_details`; do
  a=`cut -d '_' -f2 <<<$i`
  b=`cut -d '=' -f1 <<<$a`
  c=`cut -d '=' -f2 <<<$a`
- sed -i "s|$b|$c|g" docker-stack.yml
+ if [[ $b == *IMAGENAME ]];
+ then
+ sed -i "s|$b|$c:$2|g" docker-stack.yml_tmp
+ fi
+ sed -i "s|$b|$c|g" docker-stack.yml_tmp
 done
 ###########starting docker stack#######
-App_status=`docker stack ps $2`
+App_status=`docker stack ps $3`
 echo $App_status
 if [ ! -z "$App_status" ];
 then
-  docker stack rm $2
+  docker stack rm $3
 fi
-
