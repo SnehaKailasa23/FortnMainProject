e1adf2a4102a98cb78d33425a4d2286951ba64f8
GGK - Yerriswamy Konanki <GGK-ykonanki@users.noreply.padlgithubggk1.sw.fortna.net>
2018-01-04 16:10:47 +0530

    Changed the code for testing

--- RFW/run.sh
+++ RFW/run.sh
@@ -2,15 +2,15 @@
 count=0
 while ! nc -z cpservice 8080 
 do
- if [ $count -le 180 ] 
- then
+ //if [ $count -le 180 ] 
+ //then
   echo sleeping
   sleep 5
-  count=`expr $count + 1`
- fi
+  //count=`expr $count + 1`
+ //fi
 done
-if [ $count -le 180 ]; then
+//if [ $count -le 180 ]; then
    sh /robot/robot.sh cpserver 8080 cicd_sample/cicd_sample@52.67.80.187:1521/GGKF
-else
-   exit 1
-fi
+//else
+  // exit 1
+//fi
