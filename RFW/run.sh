f83463fd4ab5949462c9854d7f89e93d86df67d6
GGK - Yerriswamy Konanki <GGK-ykonanki@users.noreply.padlgithubggk1.sw.fortna.net>
2018-01-04 16:53:22 +0530

    Update run.sh

--- RFW/run.sh
+++ RFW/run.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-while ! nc -z cpservice 8080;
-        do
-          echo sleeping;
-          sleep 5;
-        done;
-        echo connected;
-sh /robot/robot.sh cpserver 8080 cicd_sample/cicd_sample@52.67.80.187:1521/GGKF
+count=0
+while ! nc -z cpservice 8080 
+do
+ if [ $count -le 180 ] 
+ then
+  echo sleeping
+  sleep 5
+  count=`expr $count + 1`
+ fi
+done
+echo connected
+if [ $count -le 180 ]; then
+   sh /robot/robot.sh cpserver 8080 cicd_sample/cicd_sample@52.67.80.187:1521/GGKF
+else
+   exit 1
+fi
