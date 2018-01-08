e680dd20871a08de696a244cb666dd2ff036fe03
SnehaKailasa23 <35054580+SnehaKailasa23@users.noreply.github.com>
2018-01-08 19:21:11 +0530

    Update clean_up.sh

--- clean_up.sh
+++ clean_up.sh
@@ -1,5 +1,104 @@
 #!/bin/bash
 source .env
+j=0
+for i in `grep -n "image_name" .env`; do
+Images[$j]=`cut -d '=' -f2 <<<$i`
+j=`expr $j+1`
+done
+j=0
+for i in `grep -n "container_name" .env`; do
+containers[$j]=`cut -d '=' -f2 <<<$i`
+j=`expr $j+1`
+done
+
+#################################################
+#Container_status=`docker ps | grep "$robot_container_name"`
+#echo $Container_status
+#while [ -n "$Container_status" ]
+#do
+#  echo sleeping;
+#  sleep 5s;
+#  Container_status=`docker ps | grep "$robot_container_name"`
+#done;
+#################################################
+for i in "${containers[@]}" ; do
+Container_status=`docker ps | grep "$i"`
+if [ ! -z "$Container_status" ];
+then
+   docker rm -f $i
+fi
+done
+echo "removed containers"
+##################################################
+for i in "${Images[@]}" ; do
+Image_status=`docker images | grep "$i"`
+if [ ! -z "$Image_status" ];
+then
+        docker rmi -f $Docker_Reg_Name/$i:${image_version}
+        echo $Docker_Reg_Name
+        docker rmi -f $Docker_Reg_Name/$i
+fi
+done
+echo "removed images"
+###############################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#!/bin/bash
+source .env
 #################################################
 Container_status=`docker ps | grep "$robot_container_name"`
 #echo $Container_status
