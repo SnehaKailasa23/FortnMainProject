8f611bb6153cf04794534c872327e6f869280677
SnehaKailasa23 <35054580+SnehaKailasa23@users.noreply.github.com>
2018-01-09 10:55:55 +0530

    Update clean_up.sh

--- clean_up.sh
+++ clean_up.sh
@@ -22,7 +22,7 @@ do
 done;
 #################################################
 for i in "${containers[@]}" ; do
-Container_status=`docker ps | grep "$i"`
+Container_status=`docker ps -a | grep "$i"`
 if [ ! -z "$Container_status" ];
 then
    docker rm -f $i
@@ -32,125 +32,19 @@ echo "removed containers"
 ##################################################
 for i in "${Images[@]}" ; do
 Image_status=`docker images | grep "$i"`
+echo "$Docker_Reg_Name"
+echo "$i"
+echo "$Image_status"
 if [ ! -z "$Image_status" ];
 then
-        docker rmi -f $Docker_Reg_Name/$i:${image_version}
-        echo $Docker_Reg_Name
-        docker rmi -f $Docker_Reg_Name/$i
+        if [[ $i != *"robot"* ]]; then
+                docker rmi -f $Docker_Reg_Name/$i:${image_version}
+                echo $Docker_Reg_Name
+                docker rmi -f $Docker_Reg_Name/$i
+        else
+                docker rmi -f $i
+        fi
 fi
 done
 echo "removed images"
 ###############################################
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#!/bin/bash
-source .env
-#################################################
-Container_status=`docker ps | grep "$robot_container_name"`
-#echo $Container_status
-while [ -n "$Container_status" ]
-do
-  echo sleeping;
-  sleep 5s;
-  Container_status=`docker ps | grep "$robot_container_name"`
-done;
-#################################################
-Container_status=`docker ps -a | grep "$robot_container_name"`
-
-if [ ! -z "$Container_status" ];
-then
-  docker rm -f $robot_container_name
-fi
-##################################################
-Container_status=`docker ps -a | grep "$cp_container_name"`
-
-if [ ! -z "$Container_status" ];
-then
-  docker rm -f $cp_container_name
-fi
-##################################################
-Container_status=`docker ps -a | grep "$om_container_name"`
-
-if [ ! -z "$Container_status" ];
-then
-  docker rm -f $om_container_name
-fi
-###############################################
-image_status=`docker images -a | grep "$robot_image_name"`
-
-if [ ! -z "$image_status" ];
-then
-  docker rmi -f $robot_image_name
-fi
-###############################################
-image_status=`docker images -a | grep "${Docker_Reg_Name}/${om_image_name}"`
-
-if [ ! -z "$image_status" ];
-then
-  docker rmi -f ${Docker_Reg_Name}/${om_image_name}
-  docker rmi -f ${Docker_Reg_Name}/${om_image_name}:${image_version}
-fi
-###############################################
-image_status=`docker images -a | grep "${Docker_Reg_Name}/${cp_image_name}"`
-
-if [ ! -z "$image_status" ];
-then
-  docker rmi -f ${Docker_Reg_Name}/${cp_image_name}
-  docker rmi -f ${Docker_Reg_Name}/${cp_image_name}:${image_version}
-fi
-
-echo "Removed all containers"
